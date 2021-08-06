5e3d874301d6715191f750d5ce37c2f4aef7f0c3
elifferide <76571128+elifferide@users.noreply.github.com>
2021-08-07 00:19:04 +0300

    resimsiz pdf

--- index.js
+++ index.js
@@ -696,6 +696,7 @@ writeStream.on('finish', function () {
     } ;
 
     s3.upload(params, function(err, response) {
+      console.log(response);
         console.log("pdf"+index+"gönderildi.");
     });
   })
@@ -722,7 +723,7 @@ function generateTable(doc, gelenVeri) {
       item.adress,
       item.passedTime,
       item.desc,
-      item.photoUrl
+     
     );
     generateHr(doc, position+ 50);
   }
@@ -735,7 +736,7 @@ function generateHr(doc, y) {
     .lineTo(550, y)
     .stroke();
 }
-function generateTableRow(doc, y, c1, c2, c3,c4) {
+function generateTableRow(doc, y, c1, c2, c3) {
   doc
     .fontSize(10)
     .font('Times-Bold')
@@ -750,7 +751,7 @@ function generateTableRow(doc, y, c1, c2, c3,c4) {
     .text("Description:", 50, (y))
     .font('Times-Roman')
     .text(c3,120, (y),{ width: 280})
-    .image(c4, 450, (y-60), {align: "right", width: 80,height:100 })
+
     .moveDown()
 }
 
