b1d0b91d35040a9f4ced2d6a5d46b37d5177a6cc
elifferide <76571128+elifferide@users.noreply.github.com>
2021-08-15 20:43:07 +0300

    15.08.02

--- scheduled-job.js
+++ scheduled-job.js
@@ -219,7 +219,7 @@ function generateTableRow(doc, y, c1, c2, c3,c4) {
   
   const logo = fetchImage(c4);
   
-  doc.image(logo, 0, 200);
+
       doc
       .fontSize(10)
       .font('Times-Bold')
@@ -234,7 +234,7 @@ function generateTableRow(doc, y, c1, c2, c3,c4) {
       .text("Description:", 50, (y))
       .font('Times-Roman')
       .text(c3,120, (y),{ width: 280})
-      .image(img, 450, (y-60), {align: "right", width: 80,height:100 })
+      .image(logo, 450, (y-60), {align: "right", width: 80,height:100 })
       .moveDown()
 }
     
